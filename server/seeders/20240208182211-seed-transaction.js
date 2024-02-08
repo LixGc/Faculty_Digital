41aef8a37245d1d201894673a13f01306ba39600
Felix Giancarlo <felixketiga@gmail.com>
2024-02-09 01:39:50 +0700

    feat: added models, dummy-data, and seeding

---
+++ server/seeders/20240208182211-seed-transaction.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const transactions = require("../dummy-data/transaction.json");
+    const transactionData = transactions.map((el) => {
+      el.createdAt = el.updatedAt = new Date();
+      return el;
+    });
+    await queryInterface.bulkInsert("Transactions", transactionData, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Transactions", null, {});
+  },
+};
