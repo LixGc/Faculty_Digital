e6ee653e1834d5151c8eac2fdc3fea6247b42c13
Felix Giancarlo <felixketiga@gmail.com>
2024-02-09 02:33:50 +0700

    feat: added register feature

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,21 @@
 class AuthController {
-  static async register(req, res, next) {}
+  static async register(req, res, next) {
+    try {
+      const { email, password, password2 } = req.body;
+      if (!password2) {
+        throw { name: "empty_input" };
+      }
+      if (password !== password2) {
+        throw { name: "Password doesn't match" };
+      }
+      await User.create({ email, password });
+      res.status(201).json({ message: `Account succesfully created!` });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+
   static async login(req, res, next) {}
   static async googleLogin(req, res, next) {}
 }
