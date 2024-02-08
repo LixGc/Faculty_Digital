728670b7afdcea1e675df2d610088f4b44723ad4
Felix Giancarlo <felixketiga@gmail.com>
2024-02-09 02:35:45 +0700

    feat: added login feature and jsonwebtoken

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,6 @@
+const { comparePassword } = require("../helpers/bcrypt");
+const { signToken } = require("../helpers/jwt");
+const { User } = require("../models");
 class AuthController {
   static async register(req, res, next) {
     try {
@@ -16,7 +19,30 @@ class AuthController {
     }
   }
 
-  static async login(req, res, next) {}
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      if (!email) {
+        throw { name: "invalid_email" };
+      }
+      if (!password) {
+        throw { name: "invalid_password" };
+      }
+      const user = await User.findOne({ where: { email } });
+      if (!user) {
+        throw { name: `not_valid` };
+      }
+
+      const isPasswordValid = comparePassword(password, user.password);
+      if (!isPasswordValid) {
+        throw { name: `not_valid` };
+      }
+      const access_token = signToken({ id: user.id });
+      res.status(200).json({ access_token });
+    } catch (error) {
+      next(error);
+    }
+  }
   static async googleLogin(req, res, next) {}
 }
 module.exports = AuthController;
