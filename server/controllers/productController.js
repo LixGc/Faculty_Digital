4663b76a64cfe35c9063af62809a3dcb1d5bd7e3
Felix Giancarlo <felixketiga@gmail.com>
2024-02-09 14:07:26 +0700

    feat: completed dashboard get feature, and also added revenueCounter helpers function

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,7 +1,65 @@
+const { Op } = require("sequelize");
+const {
+  countTodayRevenue,
+  countTodayProductSold,
+  countLast7DaysRevenue,
+  countPreviousWeekRevenue,
+  countLast7DaysProductSold,
+} = require("../helpers/revenueCounter");
+const { Product, Transaction } = require("../models");
 class ProductController {
-  static async revenue(req, res, next) {}
-  static async products(req, res, next) {}
-  static async productTransaction(req, res, next) {}
+  static async dashboard(req, res, next) {
+    try {
+      const option = {
+        include: [Transaction],
+      };
+      if (req.query.name && req.query.name.trim() !== "") {
+        option.where = {
+          name: {
+            [Op.iLike]: `%${req.query.name}%`,
+          },
+        };
+      }
+      let products = await Product.findAll(option);
+      let transactionHistory = await Transaction.findAll();
+      let totalProducts = products.length;
+      let totalProductSold = transactionHistory.reduce((accumulator, el) => {
+        return accumulator + el.amount;
+      }, 0);
+      let totalRevenue = transactionHistory.reduce((accumulator, el) => {
+        return accumulator + el.price;
+      }, 0);
+      let todayRevenue = countTodayRevenue(transactionHistory);
+      let todayProductSold = countTodayProductSold(transactionHistory);
+      let totalProdudctSoldLast7Days = countLast7DaysProductSold(transactionHistory);
+      let totalRevenueLast7Days = countLast7DaysRevenue(transactionHistory);
+      let totalRevenuePreviousWeek = countPreviousWeekRevenue(transactionHistory);
+      let averageLast7DaysRevenue = totalRevenueLast7Days / 7;
+      let averageLast7DaysProductSold = totalProdudctSoldLast7Days / 7;
+      averageLast7DaysRevenue = averageLast7DaysRevenue.toFixed(2);
+      averageLast7DaysProductSold = averageLast7DaysProductSold.toFixed(2);
+      let thisWeekRevenueComparedWithPreviousWeekRevenue = totalRevenueLast7Days - totalRevenuePreviousWeek;
+      let result = {
+        products,
+        transactionHistory,
+        totalProducts,
+        totalRevenue,
+        totalProductSold,
+        totalProdudctSoldLast7Days,
+        todayRevenue,
+        todayProductSold,
+        totalRevenueLast7Days,
+        totalRevenuePreviousWeek,
+        averageLast7DaysProductSold,
+        averageLast7DaysRevenue,
+        thisWeekRevenueComparedWithPreviousWeekRevenue,
+      };
+      res.json(result);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
   static async addProduct(req, res, next) {}
   static async editProduct(req, res, next) {}
   static async deleteProduct(req, res, next) {}
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 const ProductController = require("../controllers/productController");
 
-router.get("/revenue", ProductController.revenue);
-router.get("/products", ProductController.products);
-router.get("/product-transaction", ProductController.productTransaction);
+router.get("/dashboard", ProductController.dashboard);
 router.post("/add-product", ProductController.addProduct);
 router.put("/edit-product", ProductController.editProduct);
 router.delete("/delete-product", ProductController.deleteProduct);
