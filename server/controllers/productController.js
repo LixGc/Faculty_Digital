3e69e37c12ec54d831e379524fdd80298061bce0
Felix Giancarlo <felixketiga@gmail.com>
2024-02-10 23:47:20 +0700

    chore: updated and added more comparison of revenue and product revenue

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -5,6 +5,11 @@ const {
   countLast7DaysRevenue,
   countPreviousWeekRevenue,
   countLast7DaysProductSold,
+  countLast30DaysRevenue,
+  countLast60DaysRevenue,
+  countLast30DaysProductSold,
+  countLast60DaysProductSold,
+  countPreviousWeekProductSold,
 } = require("../helpers/revenueCounter");
 const { Product, Transaction } = require("../models");
 class ProductController {
@@ -21,34 +26,73 @@ class ProductController {
       }
 
       let transactionHistory = await Transaction.findAll(transactionOption);
-      let totalProductSold = transactionHistory.reduce((accumulator, el) => {
-        return accumulator + el.amount;
-      }, 0);
+
+      //Money
+
+      //This is total revenue of all time
       let totalRevenue = transactionHistory.reduce((accumulator, el) => {
         return accumulator + el.price;
       }, 0);
+      //This is today's total revenue
       let todayRevenue = countTodayRevenue(transactionHistory);
-      let todayProductSold = countTodayProductSold(transactionHistory);
-      let totalProdudctSoldLast7Days = countLast7DaysProductSold(transactionHistory);
+      //This is revenue for last 7 days ago
       let totalRevenueLast7Days = countLast7DaysRevenue(transactionHistory);
+      //This is revenue for last 7 days until last 14 days ago
       let totalRevenuePreviousWeek = countPreviousWeekRevenue(transactionHistory);
+      //This is revenue for last 30 days ago
+      let totalRevenueLast30DaysAgo = countLast30DaysRevenue(transactionHistory);
+      //This is revenue for last 30 days until last 60 days ago
+      let totalRevenueLast60Days = countLast60DaysRevenue(transactionHistory);
+      //This is average daily revenue from last 7 days ago
       let averageLast7DaysRevenue = totalRevenueLast7Days / 7;
-      let averageLast7DaysProductSold = totalProdudctSoldLast7Days / 7;
       averageLast7DaysRevenue = averageLast7DaysRevenue.toFixed(2);
-      averageLast7DaysProductSold = averageLast7DaysProductSold.toFixed(2);
+      //This is comparison between last 7 days ago with last 7 days until last 14 days ago revenue
       let thisWeekRevenueComparedWithPreviousWeekRevenue = totalRevenueLast7Days - totalRevenuePreviousWeek;
+      //This is comparison between last 30 days ago with last 30 days ago until last 60 days ago revenue
+      let last30DaysAgoComparedWithLast60DaysAgoRevenue = totalRevenueLast30DaysAgo - totalRevenueLast60Days;
+
+      //Product
+
+      // This is total product sold of all time
+      let totalProductSold = transactionHistory.reduce((accumulator, el) => {
+        return accumulator + el.amount;
+      }, 0);
+      //This is today's total product sold
+      let todayProductSold = countTodayProductSold(transactionHistory);
+      //This is last 7 days ago total product sold
+      let totalProdudctSoldLast7Days = countLast7DaysProductSold(transactionHistory);
+      //This is last 7 days ago until 14 days ago total product sold
+      let totalProductSoldPreviousWeek = countPreviousWeekProductSold(transactionHistory);
+      let totalProductSoldLast30Days = countLast30DaysProductSold(transactionHistory);
+      let totalProductSoldLast60Days = countLast60DaysProductSold(transactionHistory);
+      //This is comparison between last 7 days ago with last 7 days until last 14 days ago product sold
+      let thisWeekProductSoldComparedWithPreviousWeekRevenue = totalProdudctSoldLast7Days - totalProductSoldPreviousWeek;
+      //This is comparison between last 30 days ago with last 30 days until last 60 days ago product sold
+      let last30DaysAgoComparedWithLast60DaysAgoProductSold = totalProductSoldLast30Days - totalProductSoldLast60Days;
+      //This is average daily product sold from last 7 days ago
+      let averageLast7DaysProductSold = totalProdudctSoldLast7Days / 7;
+      averageLast7DaysProductSold = averageLast7DaysProductSold.toFixed(2);
+
       let result = {
         transactionHistory,
         totalRevenue,
-        totalProductSold,
-        totalProdudctSoldLast7Days,
         todayRevenue,
-        todayProductSold,
         totalRevenueLast7Days,
         totalRevenuePreviousWeek,
-        averageLast7DaysProductSold,
-        averageLast7DaysRevenue,
+        totalRevenueLast30DaysAgo,
+        totalRevenueLast60Days,
         thisWeekRevenueComparedWithPreviousWeekRevenue,
+        last30DaysAgoComparedWithLast60DaysAgoRevenue,
+        averageLast7DaysRevenue,
+        totalProductSold,
+        todayProductSold,
+        totalProdudctSoldLast7Days,
+        totalProductSoldPreviousWeek,
+        totalProductSoldLast30Days,
+        totalProductSoldLast60Days,
+        averageLast7DaysProductSold,
+        thisWeekProductSoldComparedWithPreviousWeekRevenue,
+        last30DaysAgoComparedWithLast60DaysAgoProductSold,
       };
       res.json(result);
     } catch (error) {
--- server/helpers/revenueCounter.js
+++ server/helpers/revenueCounter.js
@@ -5,8 +5,6 @@ const isToday = (date) => {
 // Helper function to check if a date is within the last 7 days
 function isWithinLast7Days() {
   var currentDate = new Date();
-
-  // Subtract 7 days to get the last 7 days
   var sevenDaysAgo = new Date(currentDate);
   sevenDaysAgo.setDate(currentDate.getDate() - 7);
 
@@ -17,14 +15,34 @@ const sevenDaysAgo = isWithinLast7Days();
 
 function isWithinLast14Days() {
   var currentDate = new Date();
-
-  // Subtract 14 days to get the last 14 days
   var fourteenDaysAgo = new Date(currentDate);
   fourteenDaysAgo.setDate(currentDate.getDate() - 14);
 
   return fourteenDaysAgo;
 }
 var last14DaysAgo = isWithinLast14Days();
+
+// Helper function to check if a date is within the last 30 days
+function isWithinLast30Days() {
+  var currentDate = new Date();
+  var thirtyDaysAgo = new Date(currentDate);
+  thirtyDaysAgo.setDate(currentDate.getDate() - 30);
+
+  return thirtyDaysAgo;
+}
+
+var thirtyDaysAgo = isWithinLast30Days();
+
+// Helper function to check if a date is within the last 60 days
+function isWithinLast60Days() {
+  var currentDate = new Date();
+  var sixtyDaysAgo = new Date(currentDate);
+  sixtyDaysAgo.setDate(currentDate.getDate() - 60);
+
+  return sixtyDaysAgo;
+}
+
+var sixtyDaysAgo = isWithinLast60Days();
 
 // Helper function to count today's revenue
 const countTodayRevenue = (transactions) => {
@@ -62,7 +80,7 @@ const countLast7DaysRevenue = (transactions) => {
   return totalRevenueLast7Days;
 };
 
-// helper function to count previous week's total revenue
+// helper function to count last 7 days ago until last 14 days ago revenue
 const countPreviousWeekRevenue = (transactions) => {
   let totalRevenuePreviousWeek = 0;
 
@@ -73,6 +91,30 @@ const countPreviousWeekRevenue = (transactions) => {
   });
   return totalRevenuePreviousWeek;
 };
+
+// helper function to count last 30 days total revenue
+const countLast30DaysRevenue = (transactions) => {
+  let totalRevenueLast30Days = 0;
+
+  transactions.forEach((el) => {
+    if (new Date(el.createdAt) >= thirtyDaysAgo && new Date(el.createdAt) <= new Date()) {
+      totalRevenueLast30Days += el.price;
+    }
+  });
+  return totalRevenueLast30Days;
+};
+
+// helper function to count last 30 days ago until last 60 days ago revenue
+const countLast60DaysRevenue = (transactions) => {
+  let totalRevenueLast60Days = 0;
+  transactions.forEach((el) => {
+    if (new Date(el.createdAt) >= sixtyDaysAgo && new Date(el.createdAt) <= new Date()) {
+      totalRevenueLast60Days += el.price;
+    }
+  });
+  return totalRevenueLast60Days;
+};
+
 // helper function to count last 7 days total product sold
 const countLast7DaysProductSold = (transactions) => {
   let totalProductSoldLast7Days = 0;
@@ -84,4 +126,48 @@ const countLast7DaysProductSold = (transactions) => {
   });
   return totalProductSoldLast7Days;
 };
-module.exports = { countTodayRevenue, countTodayProductSold, countLast7DaysRevenue, countPreviousWeekRevenue, countLast7DaysProductSold };
+// helper function to count last 7 days until last 14 days ago revenue
+const countPreviousWeekProductSold = (transactions) => {
+  let totalProductSoldLast14Days = 0;
+
+  transactions.forEach((el) => {
+    if (new Date(el.createdAt) < sevenDaysAgo && new Date(el.createdAt) >= last14DaysAgo) {
+      totalProductSoldLast14Days += el.price;
+    }
+  });
+  return totalProductSoldLast14Days;
+};
+// helper function to count last 30 days product sold
+const countLast30DaysProductSold = (transactions) => {
+  let totalProductSoldLast30Days = 0;
+
+  transactions.forEach((el) => {
+    if (new Date(el.createdAt) >= thirtyDaysAgo && new Date(el.createdAt) <= new Date()) {
+      totalProductSoldLast30Days += el.price;
+    }
+  });
+  return totalProductSoldLast30Days;
+};
+
+// helper function to count last 30 days ago until last 60 days ago revenue
+const countLast60DaysProductSold = (transactions) => {
+  let totalProductSoldLast60Days = 0;
+  transactions.forEach((el) => {
+    if (new Date(el.createdAt) >= sixtyDaysAgo && new Date(el.createdAt) <= new Date()) {
+      totalProductSoldLast60Days += el.price;
+    }
+  });
+  return totalProductSoldLast60Days;
+};
+module.exports = {
+  countTodayRevenue,
+  countTodayProductSold,
+  countLast7DaysRevenue,
+  countPreviousWeekRevenue,
+  countLast7DaysProductSold,
+  countPreviousWeekProductSold,
+  countLast30DaysRevenue,
+  countLast60DaysRevenue,
+  countLast30DaysProductSold,
+  countLast60DaysProductSold,
+};
